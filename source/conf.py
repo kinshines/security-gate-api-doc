c7cbaa2eabece71c27c0d0f3a284ea97557216fe
kinshines <1176807665@qq.com>
2018-10-18 09:03:52 +0800

    conf

--- source/conf.py
+++ source/conf.py
@@ -111,11 +111,15 @@ htmlhelp_basename = 'security-gate-apidoc'
 latex_elements = {
     # The paper size ('letterpaper' or 'a4paper').
     #
-    # 'papersize': 'letterpaper',
+     'papersize': 'a4paper',
 
     # The font size ('10pt', '11pt' or '12pt').
     #
-    # 'pointsize': '10pt',
+     'pointsize': '12pt',
+     'classoptions':',oneside',
+     'babel':'',
+     'inputenc':'',
+     'utf8extra':''
 
     # Additional stuff for the LaTeX preamble.
     #
